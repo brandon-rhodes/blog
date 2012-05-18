0f9742e0737441cb6339eed1a0e1d0c0fd4bcfa0
Brandon Rhodes <brandon@rhodesmill.org>
2012-05-18 19:23:45 -0400

    Added code to correctly format program listings in Apress EPUB chapters.

--- _filters/apress_fix.py
+++ _filters/apress_fix.py
@@ -1,6 +1,8 @@
 import re
 
-def fixup_codeblock(match):
+codeblock_re = r'(?s)<div class="pl">\n<code>(.*?)</code>\n</div>'
+
+def codeblock_fix(match):
     text = match.group(1).strip()
     text = text.replace('<br />', '')
     lang = ('html' if text.startswith(('<', '&lt;'))
@@ -8,11 +10,25 @@ def fixup_codeblock(match):
             else 'python')
     return '<pre>\n#!{}\n{}</pre>'.format(lang, text)
 
+listing_re = (
+    r'(?s)(<div id="list_[\d_]+" class="listing">\n'
+    r'<p class="normal">.*?</p>\n)'
+    r'.*?Chapter (\d+) - (.*?)<br />'
+    r'.*?'
+    r'</div>'
+    )
+
+def listing_fix(match):
+    chapter = match.group(2)
+    filename = match.group(3)
+    path = '_listings_fopnp/python2/{}/{}'.format(chapter, filename)
+    with open(path) as f:
+        text = f.read()
+    lang = 'python'
+    return '{}</div><pre>\n#!{}\n{}</pre>'.format(match.group(1), lang, text)
+
 def run(content):
     content = re.sub(r'<a id="page_\d+" />', '', content)
-    content = re.sub(
-        r'(?s)<div class="pl">\n<code>(.*?)</code>\n</div>',
-        fixup_codeblock,
-        content
-        )
+    content = re.sub(codeblock_re, codeblock_fix, content)
+    content = re.sub(listing_re, listing_fix, content)
     return content
