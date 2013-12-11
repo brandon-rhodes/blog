ab390361f98a4a6b8b2886c8b66c4335652e06c5
Brandon Rhodes <brandon@rhodesmill.org>
2013-12-11 17:17:00 -0500

    Simplify how blog paths are computed

--- bin/helpers.py
+++ bin/helpers.py
@@ -8,17 +8,12 @@ def read_posts():
     for line in open('cache/tags'):
         if line.startswith('#'):
             paths.add(line.strip().split()[2])
-    print paths
     posts = []
     for path in paths:
         source = open(path).read()
         post = eval(source, global_dict)
-        path = post['path']
-        cache_dir, rest = path.split('/', 1)
-        assert cache_dir == 'cache'
-        relative_name, extension = path.rsplit('.', 1)
-        assert extension == 'dict'
-        post['permalink'] = 'http://rhodesmill.org/' + relative_name
+        post['rpath'] = '/{}/'.format(
+            post['path'].split('/', 1)[1].rsplit('.', 1)[0])
         posts.append(post)
     posts.sort(key=lambda d: d['date'])
     tags = set(tag for post in posts for tag in post['tags'])
