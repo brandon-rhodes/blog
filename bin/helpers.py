0a822a79e01ad1f67831617779fc01a7881bb93a
Brandon Rhodes <brandon@rhodesmill.org>
2013-12-11 23:34:12 -0500

    Recreate my standard "All Posts Ever" page

--- bin/helpers.py
+++ bin/helpers.py
@@ -1,15 +1,35 @@
+# -*- coding: utf-8 -*-
 """Routines to help render my web site."""
 
 import datetime
 import re
 
 more_template = '<p><a href="{}">Read the full article...</a></p>'
+preview_limit = 256
 
 def truncate_at_more(body, url):
     pieces = re.split(r'<!-- *more *-->', body)
     if len(pieces) == 1:
         return body
     return pieces[0] + more_template.format(url)
+
+def simple_preview(body):
+    if '<table class="docinfo"' in body:
+        i = body.find('</table>') + len('</table>')
+    else:
+        i = body.find('</h1>') + len('</h1>')
+
+    preview = ''
+    while len(preview) < preview_limit and i < len(body):
+        j = body.find('<', i)
+        preview += body[i:j]
+        i = body.find('>', j) + 1
+
+    j = preview.find(' ', preview_limit)
+    if j != -1:
+        preview = preview[:j]
+
+    return preview.strip() + u'…'
 
 def read_posts():
     global_dict = {'datetime': datetime}
