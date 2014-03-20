30013ddf8cdbc1131fda8024378ee7821aa6751a
Brandon Rhodes <brandon@rhodesmill.org>
2014-03-19 23:27:28 -0400

    Remove "_compass" and "_css"
    I now use far simpler CSS than I did back when my blog was much more
    heavily decorated, so a preprocessor is not necessary.  Maybe color and
    fanciness will eventually come back into style and my minimal layout
    will look primitive to me and I will bring Sass back?  We'll see.

--- _compass/compass.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# compass.sh - run the "Compass" program
-
-BASE=$(dirname $(readlink -f $(which "$0")))
-cd $BASE
-export GEM_HOME=$BASE/Gem
-export RUBYLIB=$BASE/Gem/lib
-$BASE/Gem/bin/compass "$@"
