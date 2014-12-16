ca7e71ac623b20d4867b1843a1ea85304509cc36
Brandon Rhodes <brandon@rhodesmill.org>
2014-12-16 14:19:51 -0500

    Switch to new "Project" object from Contingent

--- bin/builder.py
+++ bin/builder.py
@@ -13,9 +13,11 @@ from docutils import nodes
 from glob import glob
 from jinja2 import DictLoader
 
-from contingent.builderlib import Builder
+from contingent.projectlib import Project
 # from contingent.cachelib import Cache, _absent
-from contingent.utils import looping_wait_on
+from contingent.io import looping_wait_on
+
+project = Project()
 
 dl = DictLoader({'full.tpl': """\
 {%- extends 'display_priority.tpl' -%}
@@ -54,16 +56,19 @@ height={{output.metadata['png']['height']}}
 {% endblock data_png %}
 """})
 
-def read_text_file(call, path):
+@project.task
+def read_text_file(path):
     with open(path) as f:
         return f.read()
 
-def read_binary_file(call, path):
+@project.task
+def read_binary_file(path):
     with open(path, 'rb') as f:
         return f.read()
 
-def parse(call, path):
-    source = call(read_text_file, path)
+@project.task
+def parse(path):
+    source = read_text_file(path)
     result = {}
     if path.endswith('.html'):
         if utils.detect_blogofile(source):
@@ -129,51 +134,58 @@ def parse(call, path):
 
     return result
 
-def title_of(call, path):
-    info = call(parse, path)
+@project.task
+def title_of(path):
+    info = parse(path)
     return info['title']
 
-def date_of(call, path):
-    info = call(parse, path)
+@project.task
+def date_of(path):
+    info = parse(path)
     return info['date']
 
-def needs_disqus(call, path):
-    info = call(parse, path)
+@project.task
+def needs_disqus(path):
+    info = parse(path)
     return info['needs_disqus']
 
-def body_of(call, path):
-    info = call(parse, path)
+@project.task
+def body_of(path):
+    info = parse(path)
     dirname = os.path.dirname(path)
     body = info['body']
     def format_title_reference(match):
         filename = match.group(1)
-        title = title_of(call, os.path.join(dirname, filename))
+        title = title_of(os.path.join(dirname, filename))
         return '<i>{}</i>'.format(title)
     body = re.sub(r'title_of\(([^)]+)\)', format_title_reference, body)
     return body
 
-def sorted_posts(call, paths):
-    dates = {path: call(date_of, path) for path in paths}
+@project.task
+def sorted_posts(paths):
+    dates = {path: date_of(path) for path in paths}
     dated_paths = [path for path in paths if dates[path]]
     return sorted(dated_paths, key=dates.get)
 
-def previous_post(call, paths, path):
-    paths = call(sorted_posts, paths)
+@project.task
+def previous_post(paths, path):
+    paths = sorted_posts(paths)
     i = paths.index(path)
     return paths[i - 1] if i else None
 
-def render(call, paths, path):
+@project.task
+def render(paths, path):
     #previous = call(previous_post, paths, path)
     #previous_title = 'NONE' if previous is None else call(title_of, previous)
-    body_html = call(body_of, path)
+    body_html = body_of(path)
     template = SimpleTemplate(name='layout.html', lookup=['templates'])
     html = template.render(
         add_title='<h1' not in body_html,
-        title=call(title_of, path),
+        title=title_of(path),
         previous_link=None,
         next_link=None,
         body_html=body_html,
-        needs_disqus=call(needs_disqus, path),
+        needs_disqus=needs_disqus(path),
         needs_mathjax=r'\(' in body_html,
         )
     # text = '<h1>{}</h1>\n<p>Date: {}</p>\n<p>Previous post: {}</p>\n{}'.format(
@@ -183,37 +195,39 @@ def render(call, paths, path):
     # print(text)
     return html
 
-def save_text(call, paths, path, outpath):
-    text = call(render, paths, path)
+@project.task
+def save_text(paths, path, outpath):
+    text = render(paths, path)
     with open(outpath, 'w') as f:
         f.write(text)
 
-def save_static(call, path, outpath):
-    data = call(read_binary_file, path)
+@project.task
+def save_static(path, outpath):
+    data = read_binary_file(path)
     with open(outpath, 'wb') as f:
         f.write(data)
 
-class BlogBuilder:
-    def __init__(self, verbose=False):
-        self.builder = Builder(self.compute)
-        self.verbose = verbose
+# class BlogBuilder:
+#     def __init__(self, verbose=False):
+#         self.builder = Builder(self.compute)
+#         self.verbose = verbose
 
-    def get(self, fn, *args):
-        return self.builder.get((fn, args))
+#     def get(self, fn, *args):
+#         return self.builder.get((fn, args))
 
-    def invalidate(self, fn, *args):
-        return self.builder.invalidate((fn, args))
+#     def invalidate(self, fn, *args):
+#         return self.builder.invalidate((fn, args))
 
-    def __getattr__(self, name):
-        return getattr(self.builder, name)
+#     def __getattr__(self, name):
+#         return getattr(self.builder, name)
 
-    def compute(self, task, _):
-        "Compute a task by direct invocation."
-        if self.verbose:
-            print('Computing', task)
+#     def compute(self, task, _):
+#         "Compute a task by direct invocation."
+#         if self.verbose:
+#             print('Computing', task)
 
-        fn, args = task
-        return fn(self.get, *args)
+#         fn, args = task
+#         return fn(self.get, *args)
 
 def find(base):
     for dirpath, dirnames, filenames in os.walk(base):
@@ -227,8 +241,6 @@ def main():
     if not os.path.exists(outdir):
         os.mkdir(outdir)
 
-    builder = BlogBuilder() #verbose=True)
-
     text_paths = tuple(
         []
         + glob('texts/brandon/*/*.rst')
@@ -241,18 +253,18 @@ def main():
         outpath = os.path.join(outdir, path.split('/', 1)[1])
         outpath = os.path.splitext(outpath)[0] + '/index.html'
         os.makedirs(os.path.dirname(outpath), exist_ok=True)
-        builder.get(save_text, text_paths, path, outpath)
+        save_text(text_paths, path, outpath)
 
     static_paths = tuple(find('static'))
 
     for path in static_paths:
         outpath = os.path.join(outdir, path.split('/', 1)[1])
         os.makedirs(os.path.dirname(outpath), exist_ok=True)
-        builder.get(save_static, path, outpath)
+        save_static(path, outpath)
 
     return
 
-    builder.verbose = True
+    project.verbose = True
     while True:
         print('=' * 72)
         print('Watching for files to change')
@@ -260,8 +272,8 @@ def main():
         print('=' * 72)
         print('Reloading:', ' '.join(changed_paths))
         for path in changed_paths:
-            builder.invalidate(read_text_file, path)
-        builder.rebuild()
+            project.invalidate(read_text_file, path)
+        project.rebuild()
 
 if __name__ == '__main__':
     main()
