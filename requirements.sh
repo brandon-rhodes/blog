31349831eed7cb25c2de5ee21c063b9e76072d5d
Brandon Rhodes <brandon@rhodesmill.org>
2015-05-30 15:15:59 -0400

    Switch to a requirements.sh file

---
+++ requirements.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+conda install \
+    'ipython-notebook<3' \
+    docutils==0.11 \
+    jinja2==2.7.3 \
+    pygments==1.6 \
+    pytz==2014.9 \
+    pyyaml==3.10 \
+
+pip install \
+    bottle==0.11.6 \
+
