1248fcca0ed3e1858d65e4c42366a0084caf6d51
Brandon Rhodes <brandon@rhodesmill.org>
2018-06-16 14:30:09 -0400

    Update requirements to include matplotlib

--- requirements.sh
+++ requirements.sh
@@ -2,12 +2,39 @@
 
 set -e
 
-# Start with: ,conda-env python=3.5
+# Start with: ,conda-env python=3.6
 
 conda install \
+    blas=1.0 \
+    cycler=0.10.0 \
+    dbus=1.13.2 \
+    expat=2.2.5 \
+    fontconfig=2.12.6 \
+    freetype=2.8 \
+    glib=2.56.1 \
+    gstreamer=1.14.0 \
+    icu=58.2 \
+    intel-openmp=2018.0.3-0 \
+    jpeg=9b \
+    kiwisolver=1.0.1 \
+    libgfortran \
+    libpng=1.6.34 \
+    libxcb=1.13 \
+    matplotlib=2.2.2 \
+    mkl=2018.0.3 \
+    mkl_fft=1.0.1 \
+    mkl_random=1.0.1 \
     mpmath=0.19 \
     notebook=5.0.0 \
+    numpy \
+    numpy=1.14.5 \
+    pcre=8.42 \
+    pyparsing=2.2.0 \
+    pyqt=5.9.2 \
     python=3.6 \
+    pytz=2018.4 \
+    qt=5.9.5 \
+    sip=4.19.8 \
     sympy=1.1.1 \
 
 pip install \
