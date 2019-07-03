e6e79b29a8c66726cdfe2680413b09563972ae46
Brandon Rhodes <brandon@rhodesmill.org>
2019-07-03 12:58:43 -0400

    Switch to dataclass

--- dev/generate.py
+++ dev/generate.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
 import argparse
+import datetime as dt
 import json
 import subprocess
 import sys
+from dataclasses import dataclass
+
+ISO = '%Y-%m-%dT%H:%M:%SZ'
 
 def main(argv):
     with open('test.template.html') as f:
@@ -25,8 +29,14 @@ def main(argv):
 
     icons = []
     next_mile = 0
+    splits = []
+
+    trackpoints = x.findall('.//Trackpoint')
+    datapoints = list(parse_trackpoints(trackpoints))
+    print(list(compute_mileposts(datapoints)))
 
     for t in x.findall('.//Trackpoint'):
+        time = dt.datetime.strptime(t.find('Time').text, ISO)
         alt = float(t.find('AltitudeMeters').text)
         distance = float(t.find('DistanceMeters').text)
         p = t.find('Position')
@@ -41,15 +51,59 @@ def main(argv):
                 'label': '{} mi'.format(next_mile),
             })
             next_mile += 1
+            splits.append(next_mile)
         # TODO: DistanceMeters
 
     content = content.replace('$LATLNGS', json.dumps(j))
             #print(t)
     content = content.replace('$ICONS', json.dumps(icons))
 
+    print(splits)
+
     #sys.stdout.write(content)
     with open('test.html', 'w') as f:
         f.write(content)
+
+@dataclass
+class Trackpoint(object):
+    time: dt.datetime
+    altitude_meters: float
+    distance_meters: float
+    latitude_degrees: float
+    longitude_degrees: float
+
+def parse_trackpoints(trackpoints):
+    for t in trackpoints:
+        p = t.find('Position')
+        yield Trackpoint(
+            time = dt.datetime.strptime(t.find('Time').text, ISO),
+            altitude_meters = float(t.find('AltitudeMeters').text),
+            distance_meters = float(t.find('DistanceMeters').text),
+            latitude_degrees = float(p.find('LatitudeDegrees').text),
+            longitude_degrees = float(p.find('LongitudeDegrees').text),
+        )
+
+def compute_mileposts(datapoints):
+    datapoints = list(datapoints)
+    next_mile = 1
+    previous_time = 0
+    previous_miles = 0
+    for d in datapoints:
+        miles = d.distance_meters * 0.000621371
+        if miles > next_mile:
+            # TODO: what if it jumped ahead multiple miles?
+            # print('A', next_mile - previous_miles)
+            # print('B', miles - previous_miles)
+            fraction = (next_mile - previous_miles) / (miles - previous_miles)
+            delta = d.time - previous_time
+            yield previous_time + delta * fraction, next_mile
+            print(fraction)
+            next_mile += 1
+        previous_time = d.time
+        previous_miles = miles
+
+# def compute_splits(mileposts):
+#
 
 if __name__ == '__main__':
     main(sys.argv[1:])
